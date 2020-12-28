5d7b546ee2da478582f404d1ddcd0f483fea2ad8
almaskotwal <almaskotwal2@gmail.com>
2020-12-27 17:39:25 -0800

    initial commit

---
+++ src/ArrayMethods.java
@@ -0,0 +1,29 @@
+
+
+public class ArrayMethods {
+
+	public static void main(String[] args) {
+	
+		
+		
+		int num = 4;
+		
+		while (num <= 25) {
+			
+			if(num % 3 == 0) {
+				System.out.println(num + " TEK");
+			}
+			
+			}
+	
+		
+		}
+		
+
+}
+		
+		
+		
+		
+
+ 
---
+++ src/Modulus.java
@@ -0,0 +1,13 @@
+
+public class Modulus {
+	
+	public static void main (String[] args) {
+		
+		int firstNumber = 100;
+		int secondNumber = 78;
+		int thirdNumber = 6;
+		
+		System.out.println(firstNumber * secondNumber/thirdNumber);
+	}
+
+}
